98c3ce9cb35b51aaaad635b3cb789e29c3abd527
Hans Schuell <hans.schuell@datev.de>
2019-02-16 11:30:33 +0100

    Minor config changes (.gitignore, yo-rc, index.sql)

--- SQL/index.sql
+++ SQL/index.sql
@@ -7,3 +7,8 @@ CREATE INDEX emname_name_index
     ON public.employee_name USING btree
     (name_key COLLATE pg_catalog."default", name_value COLLATE pg_catalog."default")
     TABLESPACE pg_default;
+
+CREATE INDEX employee_dob_index
+    ON public.employee USING btree (date_of_birth)
+    TABLESPACE pg_default;
+
