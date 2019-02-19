cd0adadb4b9f354ed24b0fa927a230aea5e726af
Hans Schuell <hans.schuell@datev.de>
2019-02-19 19:18:00 +0100

    analyze.sql improved

--- SQL/analyze.sql
+++ SQL/analyze.sql
@@ -7,3 +7,9 @@ ANALYZE jhi_user;
 -- select * from pg_stats where tablename = 'employee';
 -- select * from pg_stats where tablename = 'employee_name';
 -- select * from pg_stats where tablename = 'employee_name' and attname = 'name_value';
+
+-- DROP STATISTICS employee_name_stat_1
+-- CREATE STATISTICS employee_name_stat_1(dependencies) on owner_id, name_key, name_value from employee_name;
+
+-- DROP STATISTICS employee_stat_1
+-- CREATE STATISTICS employee_stat_1(dependencies) on id, company_id, date_of_birth from employee;
